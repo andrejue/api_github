6a500adf43dd70b82ddb72779f043606a9129eef
André de Souza <andre_aru010@hotmail.com>
2023-08-16 16:26:22 -0300

    .

--- src/pages/Repositories/RandomUser.jsx
+++ src/pages/Repositories/RandomUser.jsx
@@ -1,9 +1,6 @@
-import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 
 export default function RandomUser() {
-  const [isInitialLoad, setIsInitialLoad] = useState(true);
-
   const fetchRandomUserId = async () => {
     const response = await fetch("https://api.github.com/users");
     const data = await response.json();
@@ -12,9 +9,11 @@ export default function RandomUser() {
   };
 
   const fetchFullUser = async (login) => {
-    const response = await fetch(`https://api.github.com/users/${login}`);
-    const data = await response.json();
-    return data;
+    if (login != undefined) {
+      const response = await fetch(`https://api.github.com/users/${login}`);
+      const data = await response.json();
+      return data;
+    }
   };
 
   const randomUserQuery = useQuery("randomUser", fetchRandomUserId, {
@@ -37,6 +36,8 @@ export default function RandomUser() {
     return <div>Error loading random user</div>;
   }
 
+  console.log(fullUserQuery);
+
   return (
     <div>
       <h1>Random User</h1>
@@ -48,6 +49,12 @@ export default function RandomUser() {
         <div>
           <p>Login: {fullUserQuery.data.login}</p>
           <p>Name: {fullUserQuery.data.name}</p>
+          <p>City: {fullUserQuery.data.location}</p>
+          <a
+            href={`https://api.github.com/users/${fullUserQuery.data.login}/repos`}
+          >
+            Repositórios
+          </a>
           <img src={fullUserQuery.data.avatar_url} alt="" />
           <button onClick={() => randomUserQuery.refetch()}>
             Buscar Novo Usuário
